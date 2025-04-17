e4d9c95c1300ec538063ecb762cdf03cccb604b6
Nitchiani <gordiashvilinini@gmail.com>
2025-04-18 01:12:05 +0400

    styled swiper

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,18 +1,27 @@
-'use client'
-import { getDictionary } from '@/get-dictionary'
-import { useRef, useState } from 'react'
-import styled from 'styled-components'
-import 'swiper/css'
-import { Swiper, SwiperRef, SwiperSlide } from 'swiper/react'
-import Typography from './ui/Typography'
+"use client";
+import React, { useState, useRef, useEffect } from "react";
+import Typography from "./ui/Typography";
+import styled from "styled-components";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css/effect-creative";
+import "swiper/css";
+import { getDictionary } from "@/get-dictionary";
+import { motion } from "framer-motion";
+import Image from "next/image";
 
-const StyledContainer = styled.div`
-  background-color: rgb(239 153 161);
+const StyledContainer = styled(motion.div)`
   padding: 60px 20px;
   display: flex;
   justify-content: center;
   position: relative;
-`
+  transition: background-color 0.6s ease;
+`;
+
+const backgroundColors = [
+  "rgb(232 106 118)", // Raspberry
+  "rgb(239 153 161)", // Cherry
+  "rgb(241 216 115)", // Ginger
+];
 
 const StyledBox = styled.div`
   color: rgba(92, 14, 21, 1);
@@ -20,35 +29,51 @@ const StyledBox = styled.div`
   max-width: 800px;
   display: flex;
   flex-direction: column;
-  gap: 20px;
+  gap: 30px;
   align-items: center;
-`
+  position: relative;
+  z-index: 1;
+  @media (max-width: 1080px) {
+    gap: 16px;
+  }
+`;
 
 const StyledTitle = styled.div`
   text-align: center;
-  margin-bottom: 10px;
   width: 656px;
-`
+  text-transform: uppercase;
+  @media (max-width: 1080px) {
+    text-wrap: wrap;
+    width: 343px;
+    font-size: 24px;
+  }
+`;
 
 const StyledProduct = styled.div`
   text-align: center;
   font-weight: 700;
-  margin-bottom: 20px;
+  text-transform: uppercase;
   &:hover {
     text-decoration: underline;
   }
-`
+
+  @media (max-width: 768px) {
+    font-size: 14px;
+  }
+`;
 
 const StyledProducts = styled.div`
   display: flex;
   gap: 20px;
-`
+  @media (max-width: 1080px) {
+  }
+`;
 
 const SwiperWrapper = styled.div`
   width: 100%;
   max-width: 600px;
   position: relative;
-`
+`;
 
 const StyledSwiper = styled(Swiper)`
   .swiper-slide {
@@ -57,7 +82,7 @@ const StyledSwiper = styled(Swiper)`
     align-items: center;
     transition: all 0.3s ease;
   }
-`
+`;
 
 const NavButton = styled.button`
   position: absolute;
@@ -78,18 +103,45 @@ const NavButton = styled.button`
   }
 
   svg {
-    width: 15px;
-    height: 28px;
+    width: 12.14px;
+    height: 24.28px;
+    @media (max-width: 1080px) {
+      width: 6px;
+      height: 12px;
+    }
   }
-`
+`;
 
 const PrevButton = styled(NavButton)`
   left: 150px;
-`
+  @media (max-width: 1080px) {
+    left: 120px;
+  }
+`;
 
 const NextButton = styled(NavButton)`
   right: 150px;
-`
+  @media (max-width: 1080px) {
+    right: 120px;
+  }
+`;
+
+const CircleBackground = styled.div`
+  width: 456px;
+  height: 456px;
+  position: absolute;
+  top: 269px;
+  left: 50%;
+  transform: translateX(-50%);
+  border-radius: 50%;
+  z-index: 0;
+  background: radial-gradient(
+    circle,
+    rgba(255, 255, 255, 1) 0%,
+    rgba(255, 255, 255, 0) 100%
+  );
+  filter: blur(85px);
+`;
 
 const BottleImage = styled.div`
   display: flex;
@@ -97,7 +149,8 @@ const BottleImage = styled.div`
   align-items: center;
   height: 450px;
   transition: all 0.3s ease;
-
+  position: relative;
+  z-index: 2;
   img {
     transition: all 0.3s ease;
     object-fit: contain;
@@ -106,65 +159,112 @@ const BottleImage = styled.div`
   &.active img {
     width: 164px;
     height: 450px;
-    object-fit: contain;
   }
 
   &.side img {
     width: 120px;
     height: 336px;
-    opacity: 0.8;
-    object-fit: contain;
   }
-`
+
+  @media (max-width: 1080px) {
+    &.active img {
+      width: 160px;
+      height: 443px;
+    }
+    &.side img {
+      width: 86px;
+      height: 242px;
+    }
+  }
+`;
 
 const FlavorsDisplay = styled.div`
   text-align: center;
-  margin-top: 20px;
   font-weight: bold;
-`
+  position: relative;
+  z-index: 1;
+
+  @media (max-width: 1080px) {
+    font-size: 14px;
+  }
+`;
+
+const BackgroundLayer = styled(motion.div)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 0;
+  transform-origin: ${(props) => props.animationDirection};
+  pointer-events: none;
+  backface-visibility: hidden;
+`;
 
 export default function Slider({
-  dictionary
+  dictionary,
 }: {
-  dictionary: Awaited<ReturnType<typeof getDictionary>>['slider']
+  dictionary: Awaited<ReturnType<typeof getDictionary>>["slider"];
 }) {
-  const [activeIndex, setActiveIndex] = useState(1)
-  const swiperRef = useRef<SwiperRef | null>(null)
+  const [activeIndex, setActiveIndex] = useState(1);
+  const swiperRef = useRef(null);
+  const [backgroundColor, setBackgroundColor] = useState(backgroundColors[1]);
+  const [animationDirection, setAnimationDirection] = useState("left"); // Default direction
 
   const bottles = [
     {
-      src: '/assets/slider/raspKombucha.png',
-      alt: 'Raspberry Kombucha',
-      flavor: dictionary.bottleTypeRasp
+      src: "/assets/slider/raspKombucha.png",
+      alt: "Raspberry Kombucha",
+      flavor: dictionary.bottleTypeRasp,
     },
     {
-      src: '/assets/slider/cherryKombucha.png',
-      alt: 'Cherry Kombucha',
-      flavor: dictionary.bottleTypeCherry
+      src: "/assets/slider/cherryKombucha.png",
+      alt: "Cherry Kombucha",
+      flavor: dictionary.bottleTypeCherry,
     },
     {
-      src: '/assets/slider/gingerKombucha.png',
-      alt: 'Ginger Kombucha',
-      flavor: dictionary.bottleTypeGinger
-    }
-  ]
+      src: "/assets/slider/gingerKombucha.png",
+      alt: "Ginger Kombucha",
+      flavor: dictionary.bottleTypeGinger,
+    },
+  ];
 
-  const slidesData = [...bottles, ...bottles, ...bottles]
+  const slidesData = [...bottles, ...bottles, ...bottles];
 
   const handlePrev = () => {
-    if (swiperRef.current) {
-      swiperRef?.current?.swiper?.slidePrev()
+    setAnimationDirection("left");
+    if (swiperRef.current && swiperRef.current.swiper) {
+      swiperRef.current.swiper.slidePrev();
     }
-  }
+  };
 
   const handleNext = () => {
-    if (swiperRef.current) {
-      swiperRef.current?.swiper?.slideNext()
+    setAnimationDirection("right");
+    if (swiperRef.current && swiperRef.current.swiper) {
+      swiperRef.current.swiper.slideNext();
     }
-  }
+  };
+
+  useEffect(() => {
+    setBackgroundColor(backgroundColors[activeIndex % backgroundColors.length]);
+  }, [activeIndex]);
 
   return (
-    <StyledContainer>
+    <StyledContainer style={{ backgroundColor: backgroundColor }}>
+      <BackgroundLayer
+        key={`${activeIndex}-${animationDirection}`} // Key changes when direction changes
+        initial={{
+          rotateY: animationDirection === "right" ? -90 : 90,
+        }}
+        animate={{
+          rotateY: 0,
+        }}
+        transition={{ duration: 0.6, ease: "easeInOut" }}
+        style={{ backgroundColor: backgroundColor }}
+        animationDirection={animationDirection}
+      />
+      <CircleBackground />
+
       <StyledBox>
         <StyledTitle>
           <Typography variant="h2">{dictionary.title}</Typography>
@@ -181,8 +281,8 @@ export default function Slider({
         <SwiperWrapper>
           <PrevButton onClick={handlePrev}>
             <svg
-              width="15"
-              height="28"
+              width="12.14"
+              height="24.28"
               viewBox="0 0 15 28"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
@@ -198,8 +298,8 @@ export default function Slider({
           </PrevButton>
           <NextButton onClick={handleNext}>
             <svg
-              width="15"
-              height="28"
+              width="12.14"
+              height="24.28"
               viewBox="0 0 15 28"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
@@ -220,15 +320,20 @@ export default function Slider({
             centeredSlides={true}
             loop={true}
             onSlideChange={(swiper) => {
-              setActiveIndex(swiper.realIndex)
+              setActiveIndex(swiper.realIndex);
             }}
             initialSlide={1}
           >
             {slidesData.map((bottle, index) => (
               <SwiperSlide key={index}>
                 {({ isActive }) => (
-                  <BottleImage className={isActive ? 'active' : 'side'}>
-                    <img src={bottle.src} alt={bottle.alt} />
+                  <BottleImage className={isActive ? "active" : "side"}>
+                    <Image
+                      src={bottle.src}
+                      alt={bottle.alt}
+                      width={164}
+                      height={450}
+                    />
                   </BottleImage>
                 )}
               </SwiperSlide>
@@ -244,5 +349,5 @@ export default function Slider({
         </FlavorsDisplay>
       </StyledBox>
     </StyledContainer>
-  )
+  );
 }
